a48a379f571caa7b82b55b8c96752b6f70d181a3
CoutinhoElias <coutinho.elias@gmail.com>
2019-05-12 10:23:34 -0300

    Verificando problema na tabela.

--- cobradoronline/bolsa/models.py
+++ cobradoronline/bolsa/models.py
@@ -20,59 +20,59 @@ class PlanoDeContas(models.Model):
         return self.name
 
 
-# class Questions(models.Model):
-#     LEVEL_CHOICES = (
-#         ('0', 'Indefinido'),
-#         ('1', 'Dependencia'),
-#         ('2', 'Confianca'),
-#         ('3', 'Comprometimento'),
-#         ('4', 'Preditiva'),
-#         ('5', 'Comprometimento'),
-#     )
-#     question = models.CharField('Pergunta', max_length=200)
-#     level = models.CharField('Nível', max_length=15, choices=LEVEL_CHOICES, default='0')
-#
-#     class Meta:
-#         verbose_name = 'Questão'
-#         verbose_name_plural = 'Questões'
-#         ordering = ('-level',)
-#
-#     def __str__(self):
-#         return self.question
-#
-#
-# class PesquisaManager(models.Manager):
-#     def add_question(self, search_key, question):
-#         pesquisa, created = self.get_or_create(search_key=search_key, question=question)
-#         if not created:
-#             pesquisa.person = pesquisa.person
-#             pesquisa.question = pesquisa.question
-#             pesquisa.save()
-#         return pesquisa
+class Questions(models.Model):
+    LEVEL_CHOICES = (
+        ('0', 'Indefinido'),
+        ('1', 'Dependencia'),
+        ('2', 'Confianca'),
+        ('3', 'Comprometimento'),
+        ('4', 'Preditiva'),
+        ('5', 'Comprometimento'),
+    )
+    question = models.CharField('Pergunta', max_length=200)
+    level = models.CharField('Nível', max_length=15, choices=LEVEL_CHOICES, default='0')
+
+    class Meta:
+        verbose_name = 'Questão'
+        verbose_name_plural = 'Questões'
+        ordering = ('-level',)
+
+    def __str__(self):
+        return self.question
 
 
-# class Pesquisa(models.Model):
-#
-#     RESPOSTA_CHOICES = (
-#         ('V', 'Verdadeiro'),
-#         ('F', 'Falso'),
-#         ('I', 'Indefinido'),
-#     )
-#
-#     search_key = models.CharField('Chave da pesquisa', max_length=200, db_index=False)
-#     person = models.ForeignKey('person.person', related_name='Pessoa', on_delete=models.CASCADE)
-#     question = models.ForeignKey('bolsa.Questions', related_name='Pergunta', on_delete=models.CASCADE,)
-#     response = models.CharField('Resposta', max_length=1, choices=RESPOSTA_CHOICES, default='I')
-#     participation_on = models.DateField('período dapesquisa', default=timezone.now)
-#     created_on = models.DateTimeField('solicitado em', default=timezone.now)
-#
-#     objects = PesquisaManager()
-#
-#     class Meta:
-#         verbose_name = 'Pesquisa'
-#         verbose_name_plural = 'Pesquisas'
-#         unique_together = (('search_key', 'person', 'question'),)
-#         ordering = ('-participation_on',)
-#
-#     def __str__(self):
-#         return self.question
+class PesquisaManager(models.Manager):
+    def add_question(self, search_key, question):
+        pesquisa, created = self.get_or_create(search_key=search_key, question=question)
+        if not created:
+            pesquisa.person = pesquisa.person
+            pesquisa.question = pesquisa.question
+            pesquisa.save()
+        return pesquisa
+
+
+class Pesquisa(models.Model):
+
+    RESPOSTA_CHOICES = (
+        ('V', 'Verdadeiro'),
+        ('F', 'Falso'),
+        ('I', 'Indefinido'),
+    )
+
+    search_key = models.CharField('Chave da pesquisa', max_length=200, db_index=False)
+    person = models.ForeignKey('person.person', related_name='Pessoa', on_delete=models.CASCADE)
+    question = models.ForeignKey('bolsa.Questions', related_name='Pergunta', on_delete=models.CASCADE,)
+    response = models.CharField('Resposta', max_length=1, choices=RESPOSTA_CHOICES, default='I')
+    participation_on = models.DateField('período dapesquisa')
+    created_on = models.DateTimeField('solicitado em')
+
+    objects = PesquisaManager()
+
+    class Meta:
+        verbose_name = 'Pesquisa'
+        verbose_name_plural = 'Pesquisas'
+        unique_together = (('search_key', 'person', 'question'),)
+        ordering = ('-participation_on',)
+
+    def __str__(self):
+        return self.question
